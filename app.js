541e0fe7f47d74abf8531093cee5ae41de75e8a5
saadakhtaraptech <saadakhtaraptech@gmail.com>
2026-01-22 00:00:13 +0500

    img/single

---
+++ app.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+
+    function loadpage(page){
+        $.ajax({
+            url : page + ".html",
+            success : function(data){
+                $("#content").html(data)
+            }
+        })
+    }
+
+    $("#home").click(function(e){
+        e.preventDefault();
+        loadpage("index")
+    })
+
+    $("#about").click(function(e){
+        e.preventDefault();
+        loadpage("about")
+    })
+
+    $("#contact").click(function(e){
+        e.preventDefault();
+        loadpage("contact")
+    })
+
+    $("#category").click(function(e){
+        e.preventDefault();
+        loadpage("category")
+    })
+
+    $("#gallery").click(function(e){
+        e.preventDefault();
+        loadpage("gallery")
+    })
+    $("#feedback").click(function(e){
+        e.preventDefault();
+        loadpage("feedback")
+    })
+    loadpage("index")
+})
